064f05c0b76cf7883a086c5acd49cbdd8f539326
sumit Nayak <sumitnayak2006@gmail.com>
2026-02-26 22:48:44 +0530

    fix: remove static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",     // Required for static hosting
   images: {
     unoptimized: true,  // Avoids Next.js Image Optimization (not supported on static)
   },
